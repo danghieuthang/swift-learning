22027056c9c5cc6d04f2199ba2da6b19b3f7811a
danghieuthang <dhthang1998@gmail.com>
2024-05-29 10:04:04 +0700

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Thắng Đặng on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
